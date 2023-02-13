6b865791578d019ba6307b6ce30f99c2ad0b6087
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2023-02-13 22:25:16 +0700

    update handler location

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
-const { addBookHandler } = require("./handler");
+const { addNewBookHandler } = require("./handlers/postNewBookHandler");
+
 
 const routes = [
     {
         method: 'POST',
         path: '/books',
-        handler: addBookHandler,
+        handler: addNewBookHandler,
     },
 ];
 
